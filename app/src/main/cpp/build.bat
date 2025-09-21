8602333c90b53b1aaa2d8c2c7c4b2e2419b2796a
xororz <xcandlrr@gmail.com>
2025-09-22 00:35:18 +0800

    update build script

--- app/src/main/cpp/build.bat
+++ app/src/main/cpp/build.bat
@@ -8,14 +8,11 @@ cmake --build --preset android-release
 if %ERRORLEVEL% neq 0 goto :error
 
 if not exist lib mkdir lib
-xcopy /Y /I .\build\android\qnnlibs\*.so .\lib\
-if %ERRORLEVEL% neq 0 goto :error
-
-xcopy /Y /I .\build\android\bin\arm64-v8a\libstable_diffusion_core.so .\lib\
+xcopy /Y /E .\build\android\qnnlibs ..\assets\
 if %ERRORLEVEL% neq 0 goto :error
 
 if not exist ..\jniLibs\arm64-v8a mkdir ..\jniLibs\arm64-v8a
-xcopy /Y /I .\lib\*.so ..\jniLibs\arm64-v8a\
+xcopy /Y .\build\android\bin\arm64-v8a\libstable_diffusion_core.so ..\jniLibs\arm64-v8a\
 if %ERRORLEVEL% neq 0 goto :error
 
 echo Build completed successfully
